20a1ed7cc3fb2ff6f7bb6c485abb2b8c6b84d380
laderlappen <elmervermeulen@gmail.com>
2024-04-16 12:09:06 +0200

    Removed punctuation from apis

--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ import string
 
 def generate_api_key(length=32):
     """Generate a cryptographically secure random string."""
-    alphabet = string.ascii_letters + string.digits + string.punctuation
+    alphabet = string.ascii_letters + string.digits
     return ''.join(secrets.choice(alphabet) for _ in range(length))
